7fb017f56bcf43e6f6d778bbfa51631a4d832a10
DiegoBas17 <d.basili17@gmail.com>
2025-01-16 12:43:32 +0100

    LEGGI QUESTO COMMIT

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello leo!");
+        System.out.println("""
+                CIAO DIEGO BASILI, IO SONO UN HACKER E SONO RIUSCITO AD ENTRARE NEL TUO PROFILO DI GITHUB.
+                DEVO DIRE CHE HO VISTO COSA HAI NELLE TUE REPOSITORY, E DEVO DIRE CHE MI SENTO MOLTO DELUSO.
+                MA NON TI VERGOGNI AD AVERE MATERIALE PEDOPORNOGRAFICO????????????
+                TI DO 24 ORE DI TEMPO PER CANCELLARE TUTTO QUANTO, AL TERMINE DELLE QUALI PROVVEDERO
+                A MANDARE DENUNCIA ALLE AUTORITA COMPETENTI.
+                
+                HO FIDUCIA IN TE.
+                """);
     }
 }
